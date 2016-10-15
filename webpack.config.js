b2884b8dfe2bf4b3eb9e40d0fc9ee8a462606b3d
chuross <azarasi.mo.mo@gmail.com>
2016-10-15 16:48:25 +0900

    ビルド環境構築

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+const jsDir = path.resolve('src/js');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    `${jsDir}/app.js`
+  ],
+  output: {
+    path: path.resolve('public'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('index.template.html')
+    }),
+    new WebpackNotifierPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /(\.js$|\.jsx$)/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    root: [
+      jsDir
+    ],
+    extensions: ['', '.js', '.jsx']
+  }
+}
