4595039b80b83c04a690bff97cd941826d0637f3
chuross <azarasi.mo.mo@gmail.com>
2016-10-17 00:15:46 +0900

    アップロードボタン追加

--- src/js/ui/containers/Root.js
+++ src/js/ui/containers/Root.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Appbar from 'material-ui/Appbar'
 import Paper from 'material-ui/Paper'
 import Divider from 'material-ui/Divider'
+import RaisedButton from 'material-ui/RaisedButton'
 import { Toolbar } from 'material-ui/Toolbar'
 import { GridList, GridTile } from 'material-ui/GridList'
 import Subheader from 'material-ui/SubHeader'
@@ -13,17 +14,17 @@ export default class Root extends Component {
   render() {
     return (
       <div>
-        <Appbar title="LGTM画像アップローダー" />
+        <Appbar title="LGTM画像アップローダー" iconElementLeft={<span />} />
         <Paper className={styles.description_wrapper}>
           <Subheader>画像のアップロード</Subheader>
           <div className={styles.description_text}>
-            ここで画像アップロードできるようにする
+            <RaisedButton label="画像をアップロード" primary={true} />
           </div>
         </Paper>
         <Divider />
         <div className={styles.image_grid_wrapper}>
           <GridList cols={6} cellHeight={200} padding={10}>
-            <Subheader>LGTM一覧</Subheader>
+            <Subheader>過去に投稿した一覧</Subheader>
             <GridTile>
               <img src="http://image.rakuten.co.jp/art298/cabinet/panel/fukusima/227sawa1145x75.jpg" />
             </GridTile>
