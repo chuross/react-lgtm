1973ae0f7b8fce5c7a832f78ee5a46a864f54216
chuross <azarasi.mo.mo@gmail.com>
2016-10-30 02:17:39 +0900

    マークダウンをコピーするためのコンポーネントを配置した

--- front/src/js/ui/containers/images/Detail.js
+++ front/src/js/ui/containers/images/Detail.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actionCreators from 'ui/actions/images/detail'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+import styles from 'images/detail.css'
 
 @connect(state => ({
   image: state.app.images.detail.image
@@ -10,14 +13,28 @@ import * as actionCreators from 'ui/actions/images/detail'
 }))
 export default class Detail extends Component {
 
+  getMarkdownText() {
+    return this.props.image ? `![LGTM](${this.props.image.url})` : '';
+  }
+
   componentDidMount() {
     this.props.actions.fetchImage(this.props.params.id);
   }
 
   render() {
     return (
-      <div>
-        {this.props.image ? <div><img src={this.props.image.url} /></div> : ''}
+      <div className={styles.detail}>
+        <div className={styles.detail__image}>
+          <If condition={this.props.image}>
+            {this.props.image ? <div><img src={this.props.image.url} /></div> : ''}
+          </If>
+        </div>
+        <div className={styles.detail__markdown}>
+          <If condition={this.props.image}>
+            <TextField fullWidth={true} multiLine={true} value={this.getMarkdownText()} />
+            <RaisedButton className={styles.detail__markdown__copy_button} label="コピーする" primary={true} />
+          </If>
+        </div>
       </div>
     );
   }
