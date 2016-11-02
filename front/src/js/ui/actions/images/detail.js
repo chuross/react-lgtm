bec4ae48693a2c83ad0b0dd6f078ec3606528dc3
chuross <azarasi.mo.mo@gmail.com>
2016-11-03 00:49:51 +0900

    Actionに必要な引数を定義した

--- front/src/js/ui/actions/images/detail.js
+++ front/src/js/ui/actions/images/detail.js
@@ -1,5 +1,5 @@
 import { createAction } from 'redux-actions'
 
-export const fetchImage = createAction('IMAGE_DETAIL_FETCH_IMAGE');
-export const fetchImageSuccess = createAction('IMAGE_DETAIL_FETCH_IMAGE_SUCCESS');
-export const fetchImageFail = createAction('IMAGE_DETAI_FETCH_IMAGE_FAIL');
+export const fetchImage = createAction('IMAGE_DETAIL_FETCH_IMAGE', imageId => imageId);
+export const fetchImageSuccess = createAction('IMAGE_DETAIL_FETCH_IMAGE_SUCCESS', image => image);
+export const fetchImageFail = createAction('IMAGE_DETAI_FETCH_IMAGE_FAIL', error => error);
--- front/src/js/ui/actions/index/root.js
+++ front/src/js/ui/actions/index/root.js
@@ -1,9 +1,9 @@
 import { createAction } from 'redux-actions'
 
 export const fetchImages = createAction('INDEX_FETCH_IMAGES');
-export const fetchImagesSuccess = createAction('INDEX_FETCH_IMAGES_SUCCESS');
-export const fetchImagesFail = createAction('INDEX_FETCH_IMAGES_FAIL');
+export const fetchImagesSuccess = createAction('INDEX_FETCH_IMAGES_SUCCESS', images => images);
+export const fetchImagesFail = createAction('INDEX_FETCH_IMAGES_FAIL', error => error);
 
-export const uploadImage = createAction('INDEX_UPLOAD_IMAGE');
-export const uploadImageSuccess = createAction('INDEX_UPLOAD_IMAGE_SUCCESS');
-export const uploadImageFail = createAction('INDEX_UPLOAD_IMAGE_FAIL');
+export const uploadImage = createAction('INDEX_UPLOAD_IMAGE', blob => blob);
+export const uploadImageSuccess = createAction('INDEX_UPLOAD_IMAGE_SUCCESS', image => image);
+export const uploadImageFail = createAction('INDEX_UPLOAD_IMAGE_FAIL', error => error);
